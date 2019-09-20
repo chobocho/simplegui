ccd865a2aea31a689979cd69b8113271750a49d4
seunghwa.cho <chobocho99@gmail.com>
2019-09-21 01:32:06 +0900

    Add menu, button example

--- src/simplegui.py
+++ src/simplegui.py
@@ -2,10 +2,11 @@ import wx
 from simpleguiframe import *
 
 SW_TITLE = "SIMPLE GUI"
+WINDOW_SIZE = 480
 
 def main(): 
     app = wx.App()
-    frm = SimpleGuiFrame(None, title=SW_TITLE, size=(600,600))
+    frm = SimpleGuiFrame(None, title=SW_TITLE, size=(WINDOW_SIZE,WINDOW_SIZE))
     frm.Show()
     app.MainLoop()
 
--- src/simpleguiframe.py
+++ src/simpleguiframe.py
@@ -9,3 +9,28 @@ class SimpleGuiFrame(wx.Frame):
         sizer = wx.BoxSizer(wx.VERTICAL)
         sizer.Add(self.panel, 1, wx.EXPAND)
         self.SetSizer(sizer)
+
+        self._addMenubar()
+
+    def _addMenubar(self):
+        menubar = wx.MenuBar()
+        fileMenu = wx.Menu()
+        fileItem = fileMenu.Append(wx.ID_EXIT, 'Quit', 'Quit App')
+        self.Bind(wx.EVT_MENU, self.OnQuit, fileItem)
+        menubar.Append(fileMenu, '&File')
+
+        helpMenu = wx.Menu()
+        aboutItemId = wx.NewId()
+        aboutItem = helpMenu.Append(aboutItemId, 'About', 'About')
+        self.Bind(wx.EVT_MENU, self.OnAbout, aboutItem)
+        menubar.Append(helpMenu, '&Help')
+
+        self.SetMenuBar(menubar)
+
+    def OnQuit(self, event):
+        self.Close()
+
+    def OnAbout(self, event):
+        msg = 'Simeple GUI Template V0.1\nhttp://chobocho.com'
+        title = 'About'
+        wx.MessageBox(msg, title, wx.OK | wx.ICON_INFORMATION)
